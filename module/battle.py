e63fcb22672dbc476637ccd9687097569a09f4d9
FaberSid <dareka15san+GitKraken>
2019-08-02 20:50:01 +0900

    修正忘れていたので訂正

--- module/battle.py
+++ module/battle.py
@@ -9,7 +9,8 @@ r = requests.get(f'{db.CONFIG_ROOT}/Discord/FFM/assets/monsters.json')
 monsters = r.json()
 MONSTER_NUM = 50
 
-class battle(c.Cog):
+
+class Battle(c.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -25,7 +26,7 @@ class battle(c.Cog):
         battle_channel = self.bot.get_channel(in_battle_channel_id)
         if not battle_channel:  # if deleted the battle_channel
             player_hp = player_level * 5 + 50
-            db.channel.not_found(in_battle_channel_id,channel_id,user_id,player_hp)
+            db.channel.not_found(in_battle_channel_id, channel_id, user_id, player_hp)
             return player_hp, error_message
         player_hp = in_battle[1]
         if in_battle_channel_id != channel_id:
@@ -34,10 +35,12 @@ class battle(c.Cog):
             error_message = "<@{}>はもうやられている！（戦いをやり直すには「!!reset」だ）".format(user_id, )
         return player_hp, error_message
 
+
 def get_player_level(user_id, player_exp=None):
     if player_exp:
         return int(math.sqrt(player_exp))
     return int(math.sqrt(db.player.experience.get(user_id)))
+
 
 def get_boss(channel_id):
     channel_status = db.boss_status.get(channel_id)
@@ -53,6 +56,7 @@ def get_boss(channel_id):
         db.boss_status.set(channel_id, monster_details[0], boss_Lv, str_calc.calc(monster["HP"]))
         channel_status = [boss_Lv, str_calc.calc(monster["HP"]), monster_details[0]]
     return channel_status
+
 
 def get_player_attack(player_level, boss_level, rand):
     if boss_level % MONSTER_NUM in [20, 40] and rand < 0.1:
@@ -72,12 +76,14 @@ def get_player_attack(player_level, boss_level, rand):
         player_attack = int(player_level * plus + 10)
     return player_attack
 
+
 def get_attack_message(user_id, player_attack, monster_name, rand):
     if player_attack == 0:
         return "<@{}>の攻撃！{}にかわされてしまった...！！".format(user_id, monster_name, )
     else:
         kaishin = "会心の一撃！" if rand > 0.96 else ""
         return "<@{}>の攻撃！{}{}に`{}`のダメージを与えた！".format(user_id, kaishin, monster_name, player_attack)
+
 
 def get_boss_attack(channel_id):
     from module import str_calc
@@ -89,6 +95,7 @@ def get_boss_attack(channel_id):
     monster.update(monster_details[1])
     monster["ATK"] = monster["ATK"].replace("boss_level", str(boss_lv))
     return str_calc.calc(monster["ATK"])
+
 
 def boss_attack_process(user_id, player_hp, player_level, monster_name, channel_id):
     boss_attack = get_boss_attack(channel_id)
@@ -105,9 +112,11 @@ def boss_attack_process(user_id, player_hp, player_level, monster_name, channel_
         return "{0}の攻撃！<@{1}>は`{2}`のダメージを受けた。\n - <@{1}>のHP:`{3}`/{4}".format(
             monster_name, user_id, boss_attack, player_hp, player_level * 5 + 50)
 
+
 def get_player_exp(user_id):
     player = db.player.experience.get(user_id)
     return player
+
 
 def win_process(channel_id, boss_level, monster_name):
     battle_members = [m for m in
@@ -143,6 +152,7 @@ def win_process(channel_id, boss_level, monster_name):
     msg="{0}を倒した！\n\n{1}は`{2}`の経験値を得た。{3}\n{4}".format(monster_name, members, exp, level_up_comment, item_get)
     return ("勝利メッセージが2000文字を超えたので表示できません" if len(msg)>2000 else msg)
 
+
 def experiment(user_id, exp):
     player_exp = db.player.experience.get(user_id)
     next_exp = player_exp + exp
@@ -152,6 +162,7 @@ def experiment(user_id, exp):
         next_level = int(math.sqrt(next_exp))
         return "<@{}>はレベルアップした！`Lv.{} -> Lv.{}`".format(user_id, current_level, next_level)
     return ""
+
 
 async def reset_battle(ctx, channel_id, level_up=False):
     db.channel.end_battle(channel_id)
@@ -169,7 +180,6 @@ async def reset_battle(ctx, channel_id, level_up=False):
     from module.str_calc import calc
     monster["HP"] = monster["HP"].replace("boss_level", str(boss_lv))
     db.boss_status.set(channel_id, monster_details[0], boss_lv,  calc(monster["HP"]))
-    await ctx.send(monster, delete_after=10)
     em = discord.Embed(title="{}が待ち構えている...！\nLv.{}  HP:{}".format(monster["name"], boss_lv, calc(monster["HP"])))
     em.set_image(url=f"{db.CONFIG_ROOT}Discord/FFM/img/{monster.get('img','404.png')}")
     await ctx.send(embed=em)
