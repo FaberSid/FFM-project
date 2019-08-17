341427586b467e4cf8c6d99b575a228d14dee3b1
FaberSid <dareka15san+GitKraken>
2019-08-18 05:01:33 +0900

    boss status cheat

--- module/cheat.py
+++ module/cheat.py
@@ -10,7 +10,11 @@ class Cheat(c.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @c.command(hidden=True, description='チャンネル内の敵に特殊魔法を打ち込む。ある特殊な権限が必要。')
+    @c.group(hidden=True, description="チートを実装した")
+    async def cheat(self, ctx):
+        pass
+
+    @cheat.command(description='チャンネル内の敵に特殊魔法を打ち込む。ある特殊な権限が必要。')
     async def kill(self, ctx):
         if all((x in USERs.get(str(ctx.author.id), [])) for x in ["Cheater", "Debugger"]):
             player_hp, error_message = await battle.Battle(self.bot).into_battle(ctx.author.id, ctx.channel.id)
@@ -22,6 +26,12 @@ class Cheat(c.Cog):
             win_message = battle.win_process(ctx.channel.id, boss_level, monster_name)
             await ctx.send(embed=Embed(description="{}\n{}".format(attack_message, win_message)))
             await battle.reset_battle(ctx, ctx.channel.id, level_up=True)
+
+    @cheat.command(description='ボスの初期状態が表示されます。表示時間は10秒。')
+    async def boss(self, ctx):
+        if all((x in USERs.get(str(ctx.author.id), [])) for x in ["Cheater", "Debugger"]):
+            boss_level, _, boss_id = battle.get_boss(ctx.channel.id)
+            await ctx.send(str(monsters.get(boss_level, boss_id)), delete_after=10)
 
 
 def setup(bot):
