6386b854893c3615a38b3f67e6ce2f89c1abbe87
FaberSid <dareka15san+GitKraken>
2019-08-22 00:01:35 +0900

    修正

--- module/shop.py
+++ module/shop.py
@@ -93,7 +93,7 @@ class Shop(c.Cog):
 
 
 def get_key_from_value(d, val):
-    keys = [k for k, v in d.items() if val == v]
+    keys = [k for k, v in d.items() if val == v["name"]]
     if keys:
         return keys[0]
     return None
