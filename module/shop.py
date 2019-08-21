6bb2f8a5a9190d810b0c9023a5d2ba0355e69e97
FaberSid <dareka15san+GitKraken>
2019-08-22 00:14:41 +0900

    修正２

--- module/shop.py
+++ module/shop.py
@@ -1,14 +1,14 @@
 import asyncio
-import math
 from discord.ext import commands as c
 from module import db, item
+
 items = item.items
 
 
 class Shop(c.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @c.group()
     async def shop(self, ctx):
         # サブコマンドが指定されていない場合、メッセージを送信する。
@@ -20,12 +20,12 @@ class Shop(c.Cog):
 
     @shop.command()
     async def rate(self, ctx):
-        item_list = "\n".join("{} : {}{}{}".format(i["name"], f"売{i['sell']}FG"*(i['sell'] is not None),
-                                                   "、" * (not(i["buy"] is None and i["sell"] is None)),
-                                                   f"買{i['buy']}FG"*(i['buy'] is not None))
-                              for i in items.values() if not(i["sell"] is None and i["buy"] is None))
+        item_list = "\n".join("{} : {}{}{}".format(i["name"], f"売{i['sell']}FG" * (i['sell'] is not None),
+                                                   "、" * (not (i["buy"] is None and i["sell"] is None)),
+                                                   f"買{i['buy']}FG" * (i['buy'] is not None))
+                              for i in items.values() if not (i["sell"] is None and i["buy"] is None))
         return await ctx.send("""販売中のアイテム：\n{}""".format(item_list))
-    
+
     @shop.command()
     async def sell(self, ctx, item_name: str = None, cnt: int = 1):
         if item_name is None:
@@ -33,13 +33,13 @@ class Shop(c.Cog):
         if cnt <= 0:
             return await ctx.send("数は1以上を指定してください")
         item_id = get_key_from_value(items, item_name)
-        s_items = [item_id, items[str(item_id)]["name"]]
+        s_items = [item_id, items[str(item_id)]["name"], items[str(item_id)]["sell"]]
         item_cnt = db.player.item.get_cnt(ctx.message.author.id, s_items[0])
         if not item_cnt:
-            return await ctx.send(f"<@{ctx.message.author.id}>は{items[str(item_id)]['name']}を持っていない")
+            return await ctx.send(f"<@{ctx.message.author.id}>は{s_items[1]}を持っていない")
         elif item_cnt < cnt:
             return await ctx.send(f"所持数が足りない")
-        msg = await ctx.send(f"{items[str(item_id)]['name']} {cnt}個を{s_items[1]*cnt}FGで売却しますか？")
+        msg = await ctx.send(f"{s_items[1]} {cnt}個を{s_items[2] * cnt}FGで売却しますか？")
         await msg.add_reaction("✅")
         await msg.add_reaction("❌")
 
@@ -47,15 +47,16 @@ class Shop(c.Cog):
             _ = (user == ctx.message.author)
             _ &= (reaction.emoji in '✅❌')
             return _
+
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=10.0, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("またのお越しをお待ちしております")
         if reaction.emoji == '✅':
             money = db.player.money.get(ctx.message.author.id)
-            db.player.item.update_cnt(ctx.message.author.id, s_items[0], item_cnt-cnt)
-            db.player.money.add(ctx.message.author.id, s_items[1]*cnt)
-            await ctx.send(f"{item} {cnt}個を{s_items[1]*cnt}FGで売却しました。\nまたのお越しをお待ちしております")
+            db.player.item.update_cnt(ctx.message.author.id, s_items[0], item_cnt - cnt)
+            db.player.money.add(ctx.message.author.id, s_items[2] * cnt)
+            await ctx.send(f"{s_items[1]} {cnt}個を{s_items[2] * cnt}FGで売却しました。\nまたのお越しをお待ちしております")
         else:
             await ctx.send("またのお越しをお待ちしております")
 
@@ -66,12 +67,12 @@ class Shop(c.Cog):
         if cnt <= 0:
             return await ctx.send("数は1以上を指定してください")
         item_id = get_key_from_value(items, item_name)
-        s_items = [item_id, items[str(item_id)]["name"]]
+        s_items = [item_id, items[str(item_id)]["name"], items[str(item_id)]["buy"]]
         money = db.player.money.get(ctx.message.author.id)
-        if money < s_items[1]*cnt:
+        if money < s_items[1] * cnt:
             return await ctx.send(f"お金が足りない")
         item = items[s_items[0]]
-        msg = await ctx.send(f"{item} {cnt}個を{math.ceil(s_items[1]*cnt*1.2)}FGで買いますか？")
+        msg = await ctx.send(f"{s_items[1]} {cnt}個を{s_items[2] * cnt}FGで買いますか？")
         await msg.add_reaction("✅")
         await msg.add_reaction("❌")
 
@@ -79,15 +80,16 @@ class Shop(c.Cog):
             _ = (user == ctx.message.author)
             _ &= (reaction.emoji in '✅❌')
             return _
+
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=10.0, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("またのお越しをお待ちしております")
         if reaction.emoji == '✅':
             item_cnt = db.player.item.get_cnt(ctx.message.author.id, s_items[0])
-            db.player.money.pay(ctx.message.author.id, math.ceil(s_items[1]*cnt*1.2))
-            db.player.item.update_cnt(ctx.message.author.id, s_items[0], item_cnt+cnt)
-            await ctx.send(f"{item} {cnt}個を{math.ceil(s_items[1]*cnt*1.2)}FGで買いました。\nまたのお越しをお待ちしております")
+            db.player.money.pay(ctx.message.author.id, s_items[2] * cnt)
+            db.player.item.update_cnt(ctx.message.author.id, s_items[0], item_cnt + cnt)
+            await ctx.send(f"{s_items[1]} {cnt}個を{s_items[2] * cnt}FGで買いました。\nまたのお越しをお待ちしております")
         else:
             await ctx.send("またのお越しをお待ちしております")
 
