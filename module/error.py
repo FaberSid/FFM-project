45adfd40f5e4db6a5c2e214034068487b79b63c9
FaberSid <dareka15san+github.desktop@gmail.com>
2020-02-05 12:12:53 +0900

    Update error.py

--- module/error.py
+++ module/error.py
@@ -15,6 +15,24 @@ class CmdNotFoundError(c.Cog):
         if isinstance(error, c.errors.DisabledCommand):
             await ctx.send(embed=Embed(description="実行したコマンドは開発中か諸事情により開発者が無効化しています"))
             return
+        if isinstance(error, c.errors.BadArgument):
+            a = traceback.format_exception(type(error), error, error.__traceback__)
+            text = ""
+            ch = self.bot.get_channel(597363081928245248)
+            for x in a:
+                x = x.replace(f"\\{getpass.getuser()}\\", "\\*\\")
+                if len(text + x) < 2000-9:
+                    text += x
+                else:
+                    await ch.send(f"```py\n{text}```")
+                    text = x
+            await ch.send(f"```py\n{text}```")
+            embed = Embed(description="発言元：[移動](https://discordapp.com/channels/"
+                                    f"{ctx.guild.id}/{ctx.channel.id}/{ctx.message.id})")
+            await ch.send("<@&599220739815505941>修正よろしくね！", embed=embed)
+            await ch.send(f"{dir(error)}")
+            await ctx.send("発生したエラーは開発者が調査中です")
+            return
         a = traceback.format_exception(type(error), error, error.__traceback__)
         text = ""
         ch = self.bot.get_channel(597363081928245248)
