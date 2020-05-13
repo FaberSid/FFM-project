d84a026085fd2de9bf5ef6209cd3b59d39f75c5d
FaberSid <dareka15san+gitkraken@gmail.com>
2020-05-13 17:04:28 +0900

    Update db.py

--- module/db.py
+++ module/db.py
@@ -300,7 +300,7 @@ class channel:
         conn = psycopg2.connect(os.environ.get('DATABASE_URL_ffm'))
         c = conn.cursor()
         c.execute("DELETE FROM in_battle WHERE channel_id=%s", (b_ch,))
-        c.execute("INSERT INTO in_battle values(%s,%s,%s,0)", (user_id, a_ch, player_hp))
+        c.execute("INSERT INTO in_battle values(%s,%s,%s)", (user_id, a_ch, player_hp))
         conn.commit()
 
     @staticmethod
