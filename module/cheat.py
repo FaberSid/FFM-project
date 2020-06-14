e10f510553346c9d4090f0739232d2b447756097
FaberSid <dareka15san+gitkraken@gmail.com>
2020-06-15 00:22:37 +0900

    _

--- module/cheat.py
+++ module/cheat.py
@@ -21,17 +21,17 @@ class Cheat(c.Cog):
             player_hp, error_message = await battle.Battle(self.bot).into_battle(ctx.author.id, ctx.channel.id)
             if error_message:
                 return await ctx.send(error_message)
-            boss_level, boss_hp, boss_id = battle.get_boss(ctx.channel.id)
+            boss_level, boss_hp, boss_id = battle.get_boss(ctx)
             monster_name = monsters.get(boss_level, boss_id)[1]["name"]
             attack_message = battle.get_attack_message(ctx.author.id, boss_hp, monster_name, 2)
             win_message = battle.win_process(ctx.channel.id, boss_level, monster_name)
             await ctx.send(embed=Embed(description="{}\n{}".format(attack_message, win_message)))
-            await battle.reset_battle(ctx, ctx.channel.id, level_up=True)
+            await battle.reset_battle(ctx, level_up=True)
 
     @cheat.command(description='ボスの初期状態が表示されます。表示時間は10秒。')
     async def boss(self, ctx):
         if all((x in USERs.get(str(ctx.author.id), [])) for x in ["Cheater", "Debugger"]):
-            boss_level, _, boss_id = battle.get_boss(ctx.channel.id)
+            boss_level, _, boss_id = battle.get_boss(ctx)
             await ctx.send(str(monsters.get(boss_level, boss_id)), delete_after=10)
     
     @cheat.command(description='経験値配布などで使います。')
