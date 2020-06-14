18ac8e65482456b761eb84ba09aedab72394a69d
FaberSid <dareka15san+gitkraken@gmail.com>
2020-06-14 16:23:48 +0900

    Update error.py

--- module/error.py
+++ module/error.py
@@ -4,7 +4,8 @@ import math
 import traceback
 
 import aiohttp
-from discord import AsyncWebhookAdapter, Embed, File, Webhook
+from discord import (AsyncWebhookAdapter, Embed, File, RawReactionActionEvent,
+                     Webhook)
 from discord.ext import commands as c
 
 from module import item, status
@@ -26,9 +27,9 @@ class Cog(c.Cog):
 
     @c.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, c.errors.CommandNotFound):
+        if any([isinstance(error, i) for i in [c.CommandNotFound,c.BadArgument]]):
             return
-        elif isinstance(error, c.errors.DisabledCommand):
+        elif isinstance(error, c.DisabledCommand):
             await ctx.send(embed=Embed(description="実行したコマンドは開発中か諸事情により開発者が無効化しています"))
             return
         l_error = traceback.format_exception(
@@ -36,15 +37,17 @@ class Cog(c.Cog):
         l_error = [x.replace(f"\\{getpass.getuser()}\\", "\\*\\")
                    for x in l_error if "site-packages" not in x]
         webhook = await self.bot.fetch_webhook(712268338189041730)
-        cnt = 1
+        cnt = None
         hash_error = hashlib.sha512(
             bytes("".join(l_error), 'shift-jis')).hexdigest()
 
         async for message in webhook.channel.history(limit=None):
             if message.embeds:
                 if message.embeds[0].footer.text == hash_error and message.embeds[0].author.name:
-                    cnt = 1 + int(message.embeds[0].author.name[:-8])
-                    break
+                    if cnt is None:
+                        cnt = 1 + int(message.embeds[0].author.name[:-8])
+                    await message.delete()
+        cnt = cnt or 1
 
         def is_limit(embeds, description=""):
             """FIELD	LIMIT
@@ -89,18 +92,7 @@ class Cog(c.Cog):
             for j in i:
                 j.description = "```py\n"+j.description+"```"
         for i, embeds in enumerate(l_embeds):
-            await webhook.send(None if i else "<@&599220739815505941>修正よろしくね！", embeds=embeds)
-        """
-        [print("embeds.{}.description: {}".format(i, len(l_embeds[0][i].description)))
-         for i in range(len(l_embeds[0]))]
-        print("小計:", sum([len(i.description) for i in l_embeds[0]]))
-        [print("embeds.{}.author.name: {}".format(i, len(l_embeds[0][i].author.name)))
-         for i in range(len(l_embeds[0]))]
-        print("小計:", sum([len(i.author.name) for i in l_embeds[0]]))
-        [print("embeds.{}.footer.text: {}".format(i, len(l_embeds[0][i].footer.text)))
-         for i in range(len(l_embeds[0]))]
-        print("小計:", sum([len(i.footer.text) for i in l_embeds[0]]))
-        """
+            await webhook.send(None if i else "<@&599220739815505941>修正よろしくね！", embeds=embeds, wait=True)
         if cnt == 1:
             item.obtain_an_item(ctx.author.id, -8)
             exp = math.ceil(status.get_player_level(ctx.author.id) / 7)
@@ -109,40 +101,6 @@ class Cog(c.Cog):
         else:
             first_err_msg = ""
         await ctx.send(embed=Embed(title="エラーが発生しました", description="発生したエラーは開発者が調査中です"+first_err_msg).set_footer(text="hash: "+hash_error))
-
-        """
-            text = ""
-            ch = self.bot.get_channel(597363081928245248)
-            for x in a:
-                x = x.replace(f"\\{getpass.getuser()}\\", "\\*\\")
-                if len(text + x) < 2000-9:
-                    text += x
-                else:
-                    await ch.send(f"```py\n{text}```")
-                    text = x
-            await ch.send(f"```py\n{text}```")
-            embed = Embed(description=f"発言元：[移動]({ctx.message.jump_url})\n"
-                                      f"チャンネル：<#{ctx.channel.id}>\n")
-            await ch.send("<@&599220739815505941>修正よろしくね！", embed=embed)
-            await ch.send(f"```{error.args}\n{dir(error.args)}```")
-            await ctx.send("発生したエラーは開発者が調査中です")
-            return
-        a = traceback.format_exception(type(error), error, error.__traceback__)
-        text = ""
-        ch = self.bot.get_channel(597363081928245248)
-        for x in a:
-            x = x.replace(f"\\{getpass.getuser()}\\", "\\*\\")
-            if len(text + x) < 2000-9:
-                text += x
-            else:
-                await ch.send(f"```py\n{text}```")
-                text = x
-        await ch.send(f"```py\n{text}```")
-        embed = Embed(description="発言元：[移動](https://discordapp.com/channels/"
-                                  f"{ctx.guild.id}/{ctx.channel.id}/{ctx.message.id})")
-        await ch.send("<@&599220739815505941>修正よろしくね！", embed=embed)
-        await ctx.send("発生したエラーは開発者に報告されました。")
-        """
 
 
 def setup(bot):
